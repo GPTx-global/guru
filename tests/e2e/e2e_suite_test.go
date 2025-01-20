ae05e84205f640a7358c91ea96a019237c33a08f
Eddy <comnic@gurufin.com>
2025-01-20 14:08:29 +0900

    feat: Modidy e2e test and Remove test upgrade folder

--- tests/e2e/e2e_suite_test.go
+++ tests/e2e/e2e_suite_test.go
@@ -4,33 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
-
-	"github.com/GPTx-global/guru/tests/e2e/upgrade"
 )
 
-const (
-	// defaultManagerNetwork defines the network used by the upgrade manager
-	defaultManagerNetwork = "guru-local"
-
-	// blocksAfterUpgrade defines how many blocks must be produced after an upgrade is
-	// considered successful
-	blocksAfterUpgrade = 5
-
-	// relatedBuildPath defines the path where the build data is stored
-	relatedBuildPath = "../../build/"
-
-	// registryDockerFile builds the image using the docker image registry
-	registryDockerFile = "./upgrade/Dockerfile.init"
-
-	// repoDockerFile builds the image from the repository (used when the images are not pushed to the registry, e.g. main)
-	repoDockerFile = "./Dockerfile.repo"
-)
+const ()
 
 type IntegrationTestSuite struct {
 	suite.Suite
-
-	upgradeManager *upgrade.Manager
-	upgradeParams  upgrade.Params
 }
 
 func TestIntegrationTestSuite(t *testing.T) {
